ade785bb53bcc7877b6092b12f490738e0a4db2c
jmcl94 <jamiemclaren94@gmail.com>
2016-03-09 19:22:59 +0000

    jamietest

--- CERBookingSystem/Controllers/HomeController.cs
+++ CERBookingSystem/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace CERBookingSystem.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application jamie description page.";
 
             return View();
         }
