bd6dacbeba3a738c2be6f83227a71cf6e5bd520e
jmcl94 <jamiemclaren94@gmail.com>
2016-04-04 23:11:45 +0100

    ReportingModule

--- CERBookingSystem/Models/ReportingModel.cs
+++ CERBookingSystem/Models/ReportingModel.cs
@@ -6,18 +6,28 @@ using System.Web;
 namespace CERBookingSystem.Models
 {
     
-        public class BookedTicketsReport
+    public class BookTicketReport
+    {
+        public List<BookedTicketsDetails> ListOfBookedTickets { get; set; }
+        public List<CancelledTicketsDetails> ListOfCancelledTickets { get; set; }
+    
+    }
+        public class BookedTicketsDetails
         {
 
-            public int numberOfTickets { get; set; }
+            public int numberOfTicketsBooked { get; set; }
+
+            public DateTime dateOfBooking { get; set; }
 
 
         }
 
-        public class CanceledTicketsReport
+        public class CancelledTicketsDetails
         {
 
-            public int numberOfTickets { get; set; }
+            public int numberOfTicketsCancelled { get; set; }
+
+            public DateTime dateOfCancellation { get; set; }
 
 
         }
