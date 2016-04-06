60ea0bcf2d6a12b373bab7ccf5ed4b8a4609d707
Andrew Mehaffy <andymehaffy@hotmail.com>
2016-04-06 15:40:08 +0100

    Report controller, user booking link

---
+++ CERBookingSystem/Controllers/ReportController.cs
@@ -0,0 +1,48 @@
+﻿using CERBookingSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CERBookingSystemBLL;
+
+namespace CERBookingSystem.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            BookTicketReport ticketReport = getTicketReport(DateTime.Now.AddDays(-14), DateTime.Now);
+            return View(ticketReport);
+        }
+
+        public BookTicketReport getTicketReport(DateTime from, DateTime to)
+        {
+            BookTicketReport ticketReport = new BookTicketReport();
+            ticketReport.ListOfBookedTickets = new List<BookedTicketsDetails>();
+            ticketReport.ListOfCancelledTickets = new List<CancelledTicketsDetails>();
+            while (from <= to)
+            {
+                int activeBookings = BookingBLL.numberOfActiveBookings(from);
+                ticketReport.ListOfBookedTickets.Add(new BookedTicketsDetails
+                {
+                    dateOfBooking = from,
+                    numberOfTicketsBooked = activeBookings
+                });
+                int cancelledBooking = BookingBLL.numberCancelledBookings(from);
+                ticketReport.ListOfCancelledTickets.Add(new CancelledTicketsDetails
+                {
+                    dateOfCancellation = from,
+                    numberOfTicketsCancelled = cancelledBooking
+                });
+                from = from.AddDays(1);
+            }
+            return ticketReport;
+        }
+
+
+
+
+    }
+}
